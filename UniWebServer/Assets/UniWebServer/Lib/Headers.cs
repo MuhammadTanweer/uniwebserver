fe68a2e92390e37b7bfb5bdd06f2d976b6bb9faa
Pekka Nikander <pekka.nikander@iki.fi>
2018-05-22 11:11:30 +0300

    Remove XML Docs from Headers.cs since they are not used elsewhere

--- UniWebServer/Assets/UniWebServer/Lib/Headers.cs
+++ UniWebServer/Assets/UniWebServer/Lib/Headers.cs
@@ -7,23 +7,13 @@ using System.Text;
 
 namespace UniWebServer
 {
-    /// <summary>
-    /// Headers is a collection class for HTTP style headers.
-    /// </summary>
     public class Headers
     {
-
-        /// <summary>
-        /// Add a header to the collection.
-        /// </summary>
         public void Add (string name, string value)
         {
             GetAll (name).Add (value);
         }
 
-        /// <summary>
-        /// Get the header specified by name from the collection. Returns the first value if more than one is available.
-        /// </summary>
         public string Get (string name)
         {
             List<string> header = GetAll (name);
@@ -33,9 +23,6 @@ namespace UniWebServer
             return header [0];
         }
 
-        /// <summary>
-        /// Returns true if the collection contains the header.
-        /// </summary>
         public bool Contains (string name)
         {
             List<string> header = GetAll (name);
@@ -45,9 +32,6 @@ namespace UniWebServer
             return true;
         }
 
-        /// <summary>
-        /// Gets all the values of a header.
-        /// </summary>
         public List<string> GetAll (string name)
         {
             //name = name.ToLower();
@@ -61,9 +45,6 @@ namespace UniWebServer
             return newHeader;
         }
 
-        /// <summary>
-        /// Set the specified header to have a single value.
-        /// </summary>
         public void Set (string name, object value)
         {
             List<string> header = GetAll (name);
@@ -71,9 +52,6 @@ namespace UniWebServer
             header.Add (value.ToString ());
         }
 
-        /// <summary>
-        /// Removes a header from the collection.
-        /// </summary>
         public void Pop (string name)
         {
             if (headers.ContainsKey (name)) {
@@ -81,19 +59,12 @@ namespace UniWebServer
             }
         }
 
-
-        /// <summary>
-        /// Gets the header names present in the collection.
-        /// </summary>
         public string[] Keys {
             get {
                 return headers.Keys.ToArray ();
             }
         }
 
-        /// <summary>
-        /// Removes all headers and values from the collection.
-        /// </summary>
         public void Clear ()
         {
             headers.Clear ();
